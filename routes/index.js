f43060ae7e363433735cb0faa499ff14e205fca4
Sun <orcfeng@gmail.com>
2015-05-06 10:13:29 -0700

    fix conflict

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-
+router.get('/', function(req, res) {
+	res.sendFile('index.html');
+});
 router.get('/login', function(req, res) {
     res.render('login');
 });
@@ -12,7 +14,5 @@ router.get('/orcsun', function(req, res) {
 router.get('/blog', function(req, res) {
     res.render('blog');
 });
-router.get('/', function(req, res) {
-    res.render('tour');
-});
+
 module.exports = router;
