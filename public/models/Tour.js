7db4fe7056cb15c0338c1acdb4f664f79edfb72d
Sun <orcfeng@gmail.com>
2015-05-06 10:27:36 -0700

    solve conflict issue

--- public/models/Tour.js
+++ public/models/Tour.js
@@ -7,11 +7,6 @@ define(["jquery", "backbone"],
         var Model = Backbone.Model.extend({
         	 urlRoot: '/api/tours',
   	       idAttribute: '_id',
-            // Model Constructor
-            initialize: function() {
-//         	   this.formatDepartureDate();
-            },
-
             // Default values for all of the Model attributes
             defaults: {
         		routecode:'',
@@ -57,45 +52,8 @@ define(["jquery", "backbone"],
         			note:''
         		}],
         		note:''
-            },
-
-            // Gets called automatically by Backbone when the set and/or save methods are called (Add your own logic)
-            validate: function(attrs) {
-
-<<<<<<< HEAD
             }
-//            formatDepartureDate: function() {
-//           	 
-//                var val = this.get( 'departuredate' );
-//           
-//                if ( val ) {
-//                   val = Moment( val );
-//                   this.set( 'departuredate', val.format('MM/DD/YYYY') );
-//                   console.log('formated date is :'+this.get('departuredate'));
-////                   this.set( 'when_fmt_long', val.format('MM/DD/YYYY h:mm:ss a') );
-////                   this.set( 'when_fmt_duration_ago', val.fromNow() );
-////                   this.set( 'when_fmt_ellapsed_seconds', moment.duration( moment() - val ).as('seconds') + ' seconds' );
-//                }
-//             }
-=======
-            },
-            formatDepartureDate: function() {
-           	 
-                var val = this.get( 'departuredate' );
-           
-                if ( val ) {
-                   val = Moment( val );
-                   this.set( 'departuredate', val.format('MM/DD/YYYY') );
-                   console.log('formated date is :'+this.get('departuredate'));
-//                   this.set( 'when_fmt_long', val.format('MM/DD/YYYY h:mm:ss a') );
-//                   this.set( 'when_fmt_duration_ago', val.fromNow() );
-//                   this.set( 'when_fmt_ellapsed_seconds', moment.duration( moment() - val ).as('seconds') + ' seconds' );
-                }
-             }
->>>>>>> c06b8d7fa5a5911bb5b2422db847d4e23003ed5a
-
         });
-
         // Returns the Model class
         return Model;
 
