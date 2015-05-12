4c9185a158e6dee2b91553b1b9f8ffa68fa9db80
Sun <orcfeng@gmail.com>
2015-05-12 13:00:40 -0700

    format telephone and postcode with space

--- public/views/InfoAuthorView.js
+++ public/views/InfoAuthorView.js
@@ -12,13 +12,14 @@ define([
 
         events: {
         	"keyup .telephone":"formatTel",
+        	"keyup .postcode":"formatPostcode",
         	"click .btn_info_save": "saveInfo"
         },
         formatTel: function(e){
         	e.preventDefault();
         	
         	var input = $(e.target).val();
-        	var length = input.length>13?13:input.length;
+        	var length = input.length>14?14:input.length;
         	var c = '';
         	var out = '';
         	for(var i=0;i<length;i++){
@@ -26,14 +27,30 @@ define([
         		if(i==0 && c != '('){
         			out = '(';
         		}else if(i == 4 && c != ')'){
-        			out = out+')';
-        		}else if(i == 8 && c != '-'){
+        			out = out+') ';
+        		}else if(i == 9 && c != '-'){
         			out = out+'-';
         		}
         		out = out+c;
         	}
         	
         	$(e.target).val(out);
+        },
+        formatPostcode : function(e){
+        	e.preventDefault();
+        	
+        	var input = $(e.target).val();
+        	var c = '';
+        	var out = '';
+        	for(var i=0;i<input.length;i++){
+        		c = input.substring(i,i+1);
+        		if(i==3 && c != ' '){
+        			out = out+' ';
+        		}
+        		out = out+c;
+        	}
+        	
+        	$(e.target).val(out);	
         },
         saveInfo: function(e){
         	e.preventDefault();
--- public/views/PassengerItemView.js
+++ public/views/PassengerItemView.js
@@ -19,7 +19,7 @@ define([
         	e.preventDefault();
         	
         	var input = $(e.target).val();
-        	var length = input.length>13?13:input.length;
+        	var length = input.length>14?14:input.length;
         	var c = '';
         	var out = '';
         	for(var i=0;i<length;i++){
@@ -27,8 +27,8 @@ define([
         		if(i==0 && c != '('){
         			out = '(';
         		}else if(i == 4 && c != ')'){
-        			out = out+')';
-        		}else if(i == 8 && c != '-'){
+        			out = out+') ';
+        		}else if(i == 9 && c != '-'){
         			out = out+'-';
         		}
         		out = out+c;
